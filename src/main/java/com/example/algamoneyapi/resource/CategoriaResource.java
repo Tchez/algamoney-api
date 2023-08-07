e270bd3f7604be59d0c36cbfe21c0f60b1219913
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-08-07 00:01:28 -0300

    feat: :sparkles: adicionando endpoints para consultar e adicionar uma categoria

---
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -0,0 +1,51 @@
+package com.example.algamoneyapi.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoneyapi.model.Categoria;
+import com.example.algamoneyapi.repository.CategoriaRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @GetMapping
+    public List<Categoria> getCategorias() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Categoria> getCategoria(@PathVariable Long id) {
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> createCategoria(@RequestBody Categoria entity, HttpServletResponse response) {
+        Categoria categoria = repository.save(entity);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(categoria.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoria);
+    }
+
+}
