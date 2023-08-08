3a3f68ee6816a06c9e12eb4c56bfbc6a93a507fe
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-08-08 17:48:19 -0300

    feat: :sparkles: criando endpoint para delet de pessoa + adicionando exception personalizada caso um id inexistente seja passado na request

--- src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -17,9 +17,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import org.springframework.web.servlet.tags.BindErrorsTag;
 
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
@@ -44,6 +44,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
         List<Erro> erros = criarListaDeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+            WebRequest request) {
+        String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null,
+                LocaleContextHolder.getLocale());
+        String mensagemDev = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDev));
+
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     private List<Erro> criarListaDeErros(BindingResult bindingResult) {
--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -5,13 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.algamoneyapi.event.RecursoCriadoEvent;
@@ -49,5 +52,17 @@ public class PessoaResource {
         publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoa.getId()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(pessoa);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePessoa(@PathVariable Long id) {
+        Optional<Pessoa> pessoa = repository.findById(id);
+
+        if (!pessoa.isPresent()) {
+            throw new EmptyResultDataAccessException("Registro com ID " + id + " não encontrado.", 1);
+
+        }
+        repository.delete(pessoa.get());
     }
 }
